4703d1af7465e638fe43f14485e41e999c39a86a
aheim <antoineheim@orange.fr>
2024-07-07 18:04:52 +0200

    Update Tor container

--- tools/Tor/build-deploy.sh
+++ tools/Tor/build-deploy.sh
@@ -2,12 +2,14 @@
 
 CONTAINER_NAME=tor
 IMAGE_NAME=tor
-TAG=0.0.3
-
+TAG=0.0.4
 
 docker rm -f $CONTAINER_NAME
 docker build -t $IMAGE_NAME:$TAG .
 docker run -dit --rm --name $CONTAINER_NAME $IMAGE_NAME:$TAG
 
-docker exec -it $CONTAINER_NAME bash
+docker exec -it $CONTAINER_NAME "curl https://ifconfig.so"
+
+docker exec -it $CONTAINER_NAME "torify curl https://ifconfig.so"
+
 
